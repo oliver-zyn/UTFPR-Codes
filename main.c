02d69aa85cb10297423b1bece7de3928cdfd94c2
Vttrium <phsauthier@yahoo.com>
2023-05-17 21:06:11 -0300

    Teste

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main(void) {
+    
+    char[] sim = "cu";
+    
+    printf("%s", sim);
+}
+
