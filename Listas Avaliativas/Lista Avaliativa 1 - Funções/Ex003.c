437ac37403b6e9ef556731aab726aaab4c2e39cf
Vttrium <phsauthier@yahoo.com>
2023-05-18 22:06:32 -0300

    Ex e Funcao

--- Listas Avaliativas/Lista Avaliativa 1 - Funções/Ex003.c
+++ Listas Avaliativas/Lista Avaliativa 1 - Funções/Ex003.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#include "quadrado.h"
+
+void calc(int num);
+void calclimite(int inferior, int superior);
 
 int main() {
     int i, numLinhas, numColunas;
@@ -24,4 +26,33 @@ int main() {
     } while (continuar == 'S' || continuar == 's');
 
     return 0;
+}
+
+void calc(int num) {
+    
+    int i = 1, n3 = 0;
+    
+    for (i = 1; n3 < num; i++) {
+        n3 = i * (i + 1) * (i + 2);
+    }
+    
+    if (n3 == num) {
+        printf("%d eh triangular ==> %d * %d * %d = %d", num, i - 1, i, i + 1, n3);
+    } else {
+        printf("%d nao eh triangular.", num);
+    }
+}
+
+void calclimite(int inferior, int superior) {
+    
+    int i = 1, n3 = 0, j;
+    
+    for (inferior; inferior <= superior; inferior++) {
+        for (i; n3 < inferior; i++) {
+            n3 = i * (i + 1) * (i + 2);
+        }
+        if (n3 == inferior) {
+            printf("%d eh triangular ==> %d * %d * %d = %d\n", inferior, i - 1, i, i + 1, n3);
+        }
+    }
 }
