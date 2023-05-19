1210ea7f81e24e0a415b5800fbfcf133c9b08523
Oliver Mayer <oliver.mayer@sponte.com.br>
2023-05-18 22:07:26 -0300

    ajuste funcoes ex 01 e ex 02

--- Listas Avaliativas/Lista Avaliativa 1 - Funções/Ex001.c
+++ Listas Avaliativas/Lista Avaliativa 1 - Funções/Ex001.c
@@ -3,7 +3,35 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include "Triangular.h"
+
+void calc(int num) {
+    
+    int i = 1, n3 = 0;
+    
+    for (i = 1; n3 < num; i++) {
+        n3 = i * (i + 1) * (i + 2);
+    }
+    
+    if (n3 == num) {
+        printf("%d eh triangular ==> %d * %d * %d = %d", num, i - 1, i, i + 1, n3);
+    } else {
+        printf("%d nao eh triangular.", num);
+    }
+}
+
+void calclimite(int inferior, int superior) {
+    
+    int i = 1, n3 = 0, j;
+    
+    for (inferior; inferior <= superior; inferior++) {
+        for (i; n3 < inferior; i++) {
+            n3 = i * (i + 1) * (i + 2);
+        }
+        if (n3 == inferior) {
+            printf("%d eh triangular ==> %d * %d * %d = %d\n", inferior, i - 1, i, i + 1, n3);
+        }
+    }
+}
 
 int main() {
     int num, i, opcao, inferior, superior;
