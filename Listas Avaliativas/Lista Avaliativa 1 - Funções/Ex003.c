57804899c07bd78d20639745c3e5b95b76cc1d22
Oliver Mayer <oliver.mayer@sponte.com.br>
2023-05-18 22:09:36 -0300

    ajuste ex 03

--- Listas Avaliativas/Lista Avaliativa 1 - Funções/Ex003.c
+++ Listas Avaliativas/Lista Avaliativa 1 - Funções/Ex003.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-void calc(int num);
-void calclimite(int inferior, int superior);
+int quadrado(int numLinhas, int numColunas, int caractere) {
+    
+    int i, count;
+    
+    for (i = 0; i < (numColunas * numLinhas); i++) {
+        if (count == numColunas - 1) {
+            printf("%c\n", caractere);
+            count = 0;
+        } else {
+            printf("%c ", caractere);
+            count++;
+        }
+    }
+    
+}
 
 int main() {
     int i, numLinhas, numColunas;
@@ -26,33 +39,4 @@ int main() {
     } while (continuar == 'S' || continuar == 's');
 
     return 0;
-}
-
-void calc(int num) {
-    
-    int i = 1, n3 = 0;
-    
-    for (i = 1; n3 < num; i++) {
-        n3 = i * (i + 1) * (i + 2);
-    }
-    
-    if (n3 == num) {
-        printf("%d eh triangular ==> %d * %d * %d = %d", num, i - 1, i, i + 1, n3);
-    } else {
-        printf("%d nao eh triangular.", num);
-    }
-}
-
-void calclimite(int inferior, int superior) {
-    
-    int i = 1, n3 = 0, j;
-    
-    for (inferior; inferior <= superior; inferior++) {
-        for (i; n3 < inferior; i++) {
-            n3 = i * (i + 1) * (i + 2);
-        }
-        if (n3 == inferior) {
-            printf("%d eh triangular ==> %d * %d * %d = %d\n", inferior, i - 1, i, i + 1, n3);
-        }
-    }
 }
