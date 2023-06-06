0f51e0ed1e80c38402eb3e2a1c49476c379ebf92
Oliver Mayer <oliver.mayer@sponte.com.br>
2023-06-05 22:50:32 -0300

    atualizando uns codigos

--- Listas de Arrays/Lista 01 - Strings/Ex002.c
+++ Listas de Arrays/Lista 01 - Strings/Ex002.c
@@ -6,27 +6,32 @@ void limparBuffer() {
 }
 
 void quebraLinha(char string[]) {
-    
-    int i, j = 0, y;
-    char stringPalavras[50];
-    
+    int i, j = 0, primeiroEspaco = 0;
+    char palavra[50];
+
     printf("\n==== TEXTO NA VERTICAL ====\n");
-    
+
     for (i = 0; string[i] != '\0'; i++) {
+        if (primeiroEspaco == 0) {
+            while (string[i] == ' ') {
+                i++;
+            }
+        }
+
+        primeiroEspaco = 1;
+
         if (string[i] != ' ' && string[i] != '\n') {
-            stringPalavras[j] = string[i];
+            palavra[j] = string[i];
             j++;
         } else {
-            printf("%s\n", stringPalavras);
-            for (y = 0; y < j; y++) {
-                stringPalavras[y] = '\0';
-            }
+            palavra[j] = '\0';
+            printf("%s\n", palavra);
             j = 0;
         }
     }
-    
-    printf("%s", stringPalavras);
 
+    palavra[j] = '\0';
+    printf("%s", palavra);
 }
 
 int main()
--- Listas de Arrays/Lista 01 - Strings/Ex003.c
+++ Listas de Arrays/Lista 01 - Strings/Ex003.c
@@ -6,16 +6,26 @@ void limparBuffer() {
 }
 
 void copiaPrimeiraPalavra(char string[]) {
-    int i, j = 0;
+    int i, j = 0, y = 0, primeiroEspaco = 0;
     char stringPalavra[50];
     
     for (i = 0; string[i] != '\0'; i++) {
+        
+        if (primeiroEspaco == 0) {
+            while (string[i] == ' ') {
+                i++;
+            }
+        }
+        
+        primeiroEspaco = 1;
+        
         if (string[i] != ' ' && string[i] != '\n') {
             stringPalavra[j] = string[i];
             j++;
         } else {
             break;
         }
+        
     }
     
     stringPalavra[j] = '\0';
