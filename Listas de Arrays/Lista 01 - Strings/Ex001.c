ba0ce1aa67aa96b8792d4e840f1e0601dc4c64c4
Vttrium <phsauthier@yahoo.com>
2023-06-07 21:19:23 -0300

    correção

--- Listas de Arrays/Lista 01 - Strings/Ex001.c
+++ Listas de Arrays/Lista 01 - Strings/Ex001.c
@@ -1,57 +1,65 @@
-#include <stdio.h>
+ #include <stdio.h>
+//#include "strings.h"
 
-void limparBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF){}
-}
+void contaVogais(int vetVogais[], char string[])
+{
 
-
-void contaVogais(int vetVogais[], char string[]) {
-    
     int i;
-    
-    for(i=0; string[i]!='\0'; i++) {
-        if (string[i] == 'a' || string[i] == 'A') {
+
+    for(i=0; string[i]!='\0'; i++)
+    {
+        if (string[i] == 'a' || string[i] == 'A')
+        {
             vetVogais[0]++;
-        } else if (string[i] == 'e' || string[i] == 'E') {
+        }
+        else if (string[i] == 'e' || string[i] == 'E')
+        {
             vetVogais[1]++;
-        } else if (string[i] == 'i' || string[i] == 'I') {
+        }
+        else if (string[i] == 'i' || string[i] == 'I')
+        {
             vetVogais[2]++;
-        } else if (string[i] == 'o' || string[i] == 'O') {
+        }
+        else if (string[i] == 'o' || string[i] == 'O')
+        {
             vetVogais[3]++;
-        } else if (string[i] == 'u' || string[i] == 'U') {
+        }
+        else if (string[i] == 'u' || string[i] == 'U')
+        {
             vetVogais[4]++;
         }
     }
-    
-    printf("\nVOGAL\t QUANTIDADE\n");
-    printf("====================\n");
-    printf("a/A\t\t%d\n", vetVogais[0]);
-    printf("e/E\t\t%d\n", vetVogais[1]);
-    printf("i/I\t\t%d\n", vetVogais[2]);
-    printf("o/O\t\t%d\n", vetVogais[3]);
-    printf("u/U\t\t%d\n", vetVogais[4]);
+    printf("\nVOGAL\tQUANTIDADE\n");
+    printf("==================\n");
+    printf("A/a\t    %d\n", vetVogais[0]);
+    printf("E/e\t    %d\n", vetVogais[1]);
+    printf("I/i\t    %d\n", vetVogais[2]);
+    printf("O/o\t    %d\n", vetVogais[3]);
+    printf("U/u\t    %d\n", vetVogais[4]);
 }
 
 int main()
 {
     char continuar;
 
-    do {
-        
+    do
+    {
+
         char string[100];
-        int vetVogais[5];
-        
+        int vetVogais[5] = {0, 0, 0, 0, 0};
+
         printf("Informe um texto com ate 100 caracteres: ");
-        fgets(string, sizeof(string), stdin);
+        fflush(stdin);
+        gets(string);
 
         contaVogais(vetVogais, string);
 
         printf("\nDeseja repetir o programa (S/s)");
+        fflush(stdin);
         scanf("%c", &continuar);
-        limparBuffer();
 
-    } while (continuar == 'S' || continuar == 's');
+    }
+    while (continuar == 'S' || continuar == 's');
 
     return 0;
-}
+} 
