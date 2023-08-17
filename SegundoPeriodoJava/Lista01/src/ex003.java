c75cf6f8474b269d102f4da6a93e0ccf32b2a0e4
Oliver Mayer <oliver.mayer@sponte.com.br>
2023-08-17 19:25:35 -0300

    ex 03 e ex 05 java

--- SegundoPeriodoJava/Lista01/src/ex003.java
+++ SegundoPeriodoJava/Lista01/src/ex003.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ex003 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        int nFuncionarios;
+        float fSoma = 0;
+
+        System.out.print("Numero de funcionários: ");
+        nFuncionarios = scan.nextInt();
+
+        for(int i = 1; i <= nFuncionarios; i++) {
+            System.out.print("Informe o salário do " + i + "º funcionário: ");
+            fSoma += scan.nextFloat();
+        }
+
+        scan.close();
+
+        System.out.printf("A média salarial dos funcionários é: %.2f", (fSoma / nFuncionarios));
     }
 }
--- SegundoPeriodoJava/Lista01/src/ex005.java
+++ SegundoPeriodoJava/Lista01/src/ex005.java
@@ -1,5 +1,34 @@
+import javax.swing.JOptionPane;
+
 public class ex005 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        float fPeso, fAltura, fIMC;
+        String sIMCMessage;
+
+        do {
+            fPeso = Float.parseFloat(JOptionPane.showInputDialog("Informe seu peso"));
+            fAltura = Float.parseFloat(JOptionPane.showInputDialog("Informe sua altura"));
+
+            if (fPeso <= 0 || fAltura <= 0) {
+                JOptionPane.showMessageDialog(null, "Informe um valor válido!");
+            }
+        }
+        while(fPeso <= 0 || fAltura <= 0);
+
+        fIMC = (fPeso / (fAltura * fAltura));
+
+        if (fIMC < 20) {
+            sIMCMessage = "Magro";
+        } else if (fIMC < 24) {
+            sIMCMessage = "Normal";
+        } else if (fIMC < 29) {
+            sIMCMessage = "Acima do Peso";
+        } else if (fIMC < 34) {
+            sIMCMessage = "Obeso";
+        } else {
+            sIMCMessage = "Muito Obeso";
+        }
+
+        JOptionPane.showMessageDialog(null, "Peso = " + fPeso + "\nAltura = " + fAltura + "\nIMC = " + String.format("%.2f", fIMC) + "\nConsiderado: " + sIMCMessage);
     }
 }
